2263bc48540d8c640fe1b0c240f8f1f54fb91cdf
junior <mjjunior360@gmail.com>
2025-12-23 14:05:09 -0300

    Setup: Estrutura completa com pom, src e data

---
+++ Hospital/src/main/java/ufjf/dcc025/hospital/Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ufjf.dcc025.hospital;
+
+/**
+ *
+ * @author mjjun
+ */
+public class Hospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
