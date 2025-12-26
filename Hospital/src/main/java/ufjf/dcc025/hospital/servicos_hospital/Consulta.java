b8cc8fdd34274b034baf0b9b348c74eddffa82a9
Sávio Machado Lima <saviomachadolima2003@gmail.com>
2025-12-26 17:20:44 -0300

    Ajusta gestão de arquivos

--- Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ufjf.dcc025.hospital.controller;
+package ufjf.dcc025.hospital.servicos_hospital;
 
 /**
  *
  * @author mjjun
  */
-public class PacienteController {
+public class Consulta {
     
 }
