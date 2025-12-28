afdf2428b451f395a3ecd86e8b064cd7a54c2874
junior <mjjunior360@gmail.com>
2025-12-28 13:13:14 -0300

    feat(serviços): configurações iniciais das classes atestado e consulta

--- Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ufjf.dcc025.hospital.servicos_hospital;
 
-/**
- *
- * @author mjjun
- */
+import ufjf.dcc025.hospital.usuarios.médico.Medico;
+import ufjf.dcc025.hospital.usuarios.paciente.Paciente;
+
+
 public class Consulta {
+    
+    private String dia;
+    private String horario;
+    private String duracao;
+    private Medico medicoResponsavel;
+    private Paciente paciente;
+    
+    //private boolean marcada;
+    
+    public Consulta(String dia , String horario, String duracao, Medico medico, Paciente paciente){
+         
+        this.dia = dia;
+        this.horario = horario;
+        this.duracao = duracao;
+        this.paciente = paciente;
+        medicoResponsavel = medico;
+        //marcada = true;
+    }
+    
+    public String getDia(){ return dia; }
+    
+    public void alterarDia(String novoDia){
+        dia = novoDia;
+    }
+    
+    public String getHorario(){ return horario; }
+    
+    public void alterarHorario(String novoHorario){
+        horario = novoHorario;
+    }
+    
+    public String getDuracao(){ return duracao; }
+    
+    public void alterarDuracao(String novaDuracao){
+        duracao = novaDuracao;
+    }
+    
+    public Medico getMedicoResponsavel() { return medicoResponsavel; }
+    
+    public Paciente getPaciente(){ return paciente; }
+    
+    /*
+    public void desmarcar(){
+        marcada = false;
+    }
+    */
     
 }
