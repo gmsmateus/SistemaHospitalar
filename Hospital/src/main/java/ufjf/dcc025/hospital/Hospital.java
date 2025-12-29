9dbe018f4a3fecc6ab962f975c32ed92978ff246
Mateus Gomes da Silva <mateus.gomes@estudante.ufjf.br>
2025-12-28 23:28:43 -0300

    fix: estrutura inicial do hospital e medico minimo

--- Hospital/src/main/java/ufjf/dcc025/hospital/Hospital.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/Hospital.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ufjf.dcc025.hospital;
 
+import java.util.ArrayList;
+import java.util.List;
+//import ufjf.dcc025.hospital.usuarios.Usuario;
+import ufjf.dcc025.hospital.usuarios.médico.Medico;
+import ufjf.dcc025.hospital.usuarios.paciente.Paciente;
+import ufjf.dcc025.hospital.exception.UsuarioNaoEncontradoException;
 /**
- *
+ *classe central do sistema do hospital
+ *é responsavel por gerenciar o cadastro e acesso aos usuarios
  * @author mjjun
  */
 public class Hospital {
-    
+    private List<Medico> medicos;
+    private List<Paciente> pacientes;
+
+    public Hospital(){
+        this.medicos = new ArrayList<>();
+        this.pacientes = new ArrayList<>();
+    }
+
+    //Parte do cadastro
+    public void cadastrarMedico(Medico medico){
+        medicos.add(medico);
+    }
+
+    public void cadastrarPaciente(Paciente paciente){
+        pacientes.add(paciente);
+    }
+
+    //parte da busca
+    public Medico buscarMedicoPorCpf(String cpf){
+        return medicos.stream()
+                .filter(m -> m.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(()->
+                    new UsuarioNaoEncontradoException("Medico não encontrado"));
+    }
+    public Paciente buscarPacientePorCpf(String cpf){
+        return pacientes.stream()
+                .filter(p -> p.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(()->
+                    new UsuarioNaoEncontradoException("Paciente não encontrado"));
+    }
+
+    //parte da listagem
+    public List<Medico> listarMedicos(){
+        return new ArrayList<>(medicos);
+    }
+    public List<Paciente> listarPacientes(){
+        return new ArrayList<>(pacientes);
+    }
+
 }
--- Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Medico.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Medico.java
@@ -4,10 +4,21 @@
  */
 package ufjf.dcc025.hospital.usuarios.médico;
 
+import ufjf.dcc025.hospital.usuarios.Usuario;
+import ufjf.dcc025.hospital.exception.DadosInvalidosException;
+
 /**
- *
+ *representa um medico do sistema do hospital
+ *aqui é a implementação minima para uso pelo hospital
  * @author mjjun
  */
-public class Medico {
-    
+public class Medico extends Usuario{
+    public Medico(String nome, String cpf, String login, String senha) throws DadosInvalidosException {
+        super(nome, cpf, login, senha);
+    }
+
+     @Override
+    public String getTipo() {
+        return "MEDICO";
+    }
 }
