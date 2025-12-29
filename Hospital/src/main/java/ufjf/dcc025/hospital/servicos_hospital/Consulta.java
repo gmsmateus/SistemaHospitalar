132ab20fc0f2fbdf059a5544826242e73e9c746b
Mateus Gomes da Silva <mateus.gomes@estudante.ufjf.br>
2025-12-28 23:49:31 -0300

    feat: cria classe Consulta para vincular medico e paciente

--- Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/servicos_hospital/Consulta.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ufjf.dcc025.hospital.servicos_hospital;
 
+import java.time.LocalDateTime;
+import ufjf.dcc025.hospital.usuarios.médico.Medico;
+import ufjf.dcc025.hospital.usuarios.paciente.Paciente;
 /**
- *
+ *representa uma cosulta medica no hospital
  * @author mjjun
  */
 public class Consulta {
-    
+    private Medico medico;
+    private Paciente paciente;
+    private LocalDateTime dataHora;
+    private boolean realizada;
+
+    public Consulta(Medico medico, Paciente paciente, LocalDateTime dataHora){
+        this.medico = medico;
+        this.paciente = paciente;
+        this.dataHora = dataHora;
+        this.realizada = false;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public Paciente getPaciente(){
+        return paciente;
+    }
+
+    public LocalDateTime getDataHora(){
+        return dataHora;
+    }
+
+    public boolean isRealizada(){
+        return realizada;
+    }
+
+    public void marcarConcluida(){
+        this.realizada = true;
+    }
+
 }
