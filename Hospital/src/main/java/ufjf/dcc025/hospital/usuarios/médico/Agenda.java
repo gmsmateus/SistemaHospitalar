f3928ca5168c8217e2814a4b7b403dbda53c4397
Mateus Gomes da Silva <mateus.gomes@estudante.ufjf.br>
2025-12-29 11:18:58 -0300

    feat: medico passa a possuir agenda de atendimento

--- Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Agenda.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Agenda.java
@@ -1,5 +1,31 @@
 package ufjf.dcc025.hospital.usuarios.médico;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 public class Agenda {
+    private DayOfWeek dia;
+    private LocalTime horaInicio;
+    private LocalTime horaFim;
+    private int duracaoConsultas; //em minutos
     
+    public Agenda(DayOfWeek dia, LocalTime inicio, LocalTime fim, int duracaoConsulta){
+        this.dia = dia;
+        this.horaInicio = inicio;
+        this.horaFim = fim;
+        this.duracaoConsultas = duracaoConsulta;
+    }
+
+    public DayOfWeek getDia(){
+        return dia;
+    }
+    public LocalTime getHoraInicio(){
+        return horaInicio;
+    }
+    public LocalTime getHoraFim(){
+        return horaFim;
+    }
+    public int getDuracaoConsultas(){
+        return duracaoConsultas;
+    }
 }
--- Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Medico.java
+++ Hospital/src/main/java/ufjf/dcc025/hospital/usuarios/médico/Medico.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ufjf.dcc025.hospital.usuarios.médico;
 
 import ufjf.dcc025.hospital.usuarios.Usuario;
 import ufjf.dcc025.hospital.exception.DadosInvalidosException;
-
 /**
  *representa um medico do sistema do hospital
  *aqui é a implementação minima para uso pelo hospital
  * @author mjjun
  */
 public class Medico extends Usuario{
+    private Agenda agenda;
     public Medico(String nome, String cpf, String login, String senha) throws DadosInvalidosException {
         super(nome, cpf, login, senha);
+    }
+    public void definirAgenda(Agenda agenda){
+        this.agenda = agenda;
+    }
+
+    public Agenda getAgenda(){
+        return agenda;
     }
 
      @Override
